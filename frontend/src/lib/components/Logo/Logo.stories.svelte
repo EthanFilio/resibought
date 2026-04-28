8334f09fdca9f4b2929175930274ba42c1183059
RhamzHam <abrahamgsahi@gmail.com>
2026-04-28 19:13:10 +0800

    feat(logo): add props to customize logo coponent

---
+++ frontend/src/lib/components/Logo/Logo.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Logo from './Logo.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Logo/Logo',
+		component: Logo,
+		tags: ['autodocs']
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+
+<Story name="Large" args={{ size: 'large' }} />
+
+<Story name="Medium" args={{ size: 'medium' }} />
+
+<Story name="Small" args={{ size: 'small' }} />
